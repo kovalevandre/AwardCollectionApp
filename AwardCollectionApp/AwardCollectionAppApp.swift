32f1c80aa9c19806064c596445463263dc0e3c66
Andrey <akovalev@me.com>
2023-12-29 04:13:22 +0300

    Initial Commit

---
+++ AwardCollectionApp/AwardCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardCollectionAppApp.swift
+//  AwardCollectionApp
+//
+//  Created by Andrey Kovalev on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AwardCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
